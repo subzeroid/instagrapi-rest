497bc6f5ee71c8b3eaf2e44f3f926f2f6a6dd809
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-05-21 21:02:20 +0300

    Added swift client as example

---
+++ swift/README.md
@@ -0,0 +1,13 @@
+## Launch as script
+
+`./client.swift`
+
+## Compile
+
+`swiftc main.swift -o kraken`
+
+or `xcrun -sdk macosx swiftc client.swift -o client`
+
+## Launch compiled version
+
+`./client`
---
+++ swift/client.swift
@@ -0,0 +1,43 @@
+#!/usr/bin/swift
+//
+//  client.swift
+//  for instagrapi-rest
+//
+//  Created by adw0rd on 21.05.2021.
+//
+
+import Foundation
+
+let BaseUrl = "http://localhost:8000"
+
+func getVersion() {
+    let url = URL(string: "\(BaseUrl)/version")!
+    let sem = DispatchSemaphore(value: 0)
+    let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+        guard let data = data else { return }
+        print("\nVersion:", String(data: data, encoding: .utf8)!)
+        sem.signal()
+    }
+    task.resume()
+    sem.wait()
+}
+
+//func pkFromCode(code: String) {
+//    // let url = URL(string: "\(BaseUrl)/media/pk_from_code")!
+//    let queryItems = URLQueryItem(name: "code", value: code)
+//    var urlComps = URLComponents(string: "\(BaseUrl)/media/pk_from_code")
+//    urlComps.queryItems = queryItems
+//    let url = urlComps.url!
+//    let sem = DispatchSemaphore(value: 0)
+//    let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+//        guard let data = data else { return }
+//        print("\npkFromCode: \(code) ->", String(data: data, encoding: .utf8)!)
+//        sem.signal()
+//    }
+//    task.resume()
+//    sem.wait()
+//}
+
+getVersion()
+//pkFromCode(code: "B1LbfVPlwIA")
+// dump(Process.arguments)
