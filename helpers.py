cb954918b157a2040941a2150b39fcf3452ad0fc
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-05-23 21:50:51 +0300

    Send photo as video (temporary)

--- helpers.py
+++ helpers.py
@@ -7,8 +7,8 @@ async def photo_upload_story(cl, content, caption, **kwargs):
     with tempfile.NamedTemporaryFile(suffix='.jpg') as fp:
         fp.write(content)
         mentions = kwargs.get('mentions') or []
-        photo = StoryBuilder(fp.name, caption, mentions).photo(15)
-        return cl.photo_upload_to_story(photo.path, caption, **kwargs)
+        video = StoryBuilder(fp.name, caption, mentions).photo(15)
+        return cl.video_upload_to_story(video.path, caption, **kwargs)
 
 
 async def video_upload_story(cl, content, caption, **kwargs):
