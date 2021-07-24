e87fe041963c661dc748efedd8905c57add3954b
bedefaced <bedefaced@protonmail.com>
2021-07-25 00:01:07 +0300

    go client update

--- golang/client.go
+++ golang/client.go
@@ -1,72 +1,145 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
-	"net/http"
-	"net/url"
-	"strconv"
-	"strings"
+	"log"
+	"os"
+
+	"github.com/go-resty/resty/v2"
 )
 
 const BaseUrl = "http://localhost:8000"
 
+var client *resty.Client
+
+func init() {
+	client = resty.New()
+	client.SetHostURL(BaseUrl)
+	client.SetContentLength(true)
+}
+
 func getVersion() string {
-	resp, err := http.Get(fmt.Sprintf("%s/version", BaseUrl))
+	resp, err := client.R().Get("/version")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	return string(body)
+	return resp.String()
 }
 
 func pkFromCode(code string) string {
-	client := &http.Client{}
-	req, _ := http.NewRequest(
-		"GET",
-		fmt.Sprintf("%s/media/pk_from_code", BaseUrl),
-		nil,
-	)
-	q := req.URL.Query()
-	q.Add("code", code)
-	req.URL.RawQuery = q.Encode()
-	resp, err := client.Do(req)
+	resp, err := client.R().
+		SetQueryParams(map[string]string{
+			"code": code,
+		}).
+		Get("/media/pk_from_code")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return string(body)
+	return resp.String()
 }
 
-func login(username string, password string) string {
-	client := &http.Client{}
-	data := url.Values{}
-	data.Set("username", username)
-	data.Set("password", password)
-	req, _ := http.NewRequest(
-		"GET",
-		fmt.Sprintf("%s/auth/login", BaseUrl),
-		strings.NewReader(data.Encode()),
-	)
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
-	resp, _ := client.Do(req)
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+func login(username string, password string, settings string) string {
+	resp, err := client.R().
+		SetFormData(map[string]string{
+			"username": username,
+			"password": password,
+			"settings": settings,
+		}).Post("/auth/login")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	return string(body)
+	if resp.StatusCode() != 200 {
+		log.Println(resp.String())
+		return ""
+	}
+	return resp.String()
+}
+
+func relogin(sessionid string) {
+	resp, err := client.R().
+		SetFormData(map[string]string{
+			"sessionid": sessionid,
+		}).Post("/auth/relogin")
+	if err != nil {
+		log.Println(err)
+	}
+	if resp.StatusCode() != 200 {
+		log.Println(resp.String())
+	}
+}
+
+func photo_download(sessionid, url, folder string) string {
+	resp, err := client.R().
+		SetFormData(map[string]string{
+			"sessionid":  sessionid,
+			"url":        url,
+			"folder":     folder,
+			"returnFile": "false",
+		}).Post("/photo/download/by_url")
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	if resp.StatusCode() != 200 {
+		log.Println(resp.String())
+		return ""
+	}
+	return resp.String()
+}
+
+func getSettings(sessionid string) string {
+	resp, err := client.R().
+		SetQueryParams(map[string]string{
+			"sessionid": sessionid,
+		}).Get("/auth/settings")
+	if err != nil {
+		log.Println(err)
+	}
+	if resp.StatusCode() != 200 {
+		log.Println(resp.String())
+		return "{}"
+	}
+	return resp.String()
+}
+
+func loadSettings(file string) string {
+	if _, err := os.Stat(file); err != nil {
+		if os.IsNotExist(err) {
+			return ""
+		}
+	}
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Println(err)
+		return "{}"
+	}
+	return string(content)
+}
+
+func saveSettings(file string, settings string) {
+	fd, err := os.Create(file)
+	defer fd.Close()
+	_, err = fd.WriteString(settings)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func main() {
-	fmt.Printf("\nVersion: %s", getVersion())
-	fmt.Printf("\npkFromCode: B1LbfVPlwIA -> %s", pkFromCode("B1LbfVPlwIA"))
-	sessionid := login("adw0rd", "test")
-	fmt.Printf("\nSESSIONID: %s", sessionid)
+	log.Println("Version: ", getVersion())
+	log.Println("pkFromCode: B1LbfVPlwIA -> ", pkFromCode("B1LbfVPlwIA"))
+	settings := loadSettings("./settings.json")
+	sessionid := login("adw0rd", "test", settings)
+	if sessionid != "" {
+		log.Println("SESSIONID: ", sessionid)
+		settings = getSettings(sessionid)
+		if settings != "{}" {
+			log.Println(settings)
+			saveSettings("./settings.json", settings)
+		}
+	} else {
+		log.Fatal("Login error!")
+	}
+	// relogin(sessionid)
+	log.Println("photo_download:", photo_download(sessionid, "https://www.instagram.com/p/COQebHWhRUg/", ""))
 }
