e971b900d7f46f651faa9522486b7e6095876495
bedefaced <bedefaced@protonmail.com>
2021-07-24 23:19:13 +0300

    auth router relogin and settings control added

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,6 @@
-from typing import Optional
-
-from fastapi import APIRouter, Depends, Form
-
+import json
+from typing import Optional, Dict
+from fastapi import APIRouter, Depends, Form, File, UploadFile
 from dependencies import ClientStorage, get_clients
 
 router = APIRouter(
@@ -13,11 +12,15 @@ router = APIRouter(
 @router.post("/login")
 async def auth_login(username: str = Form(...),
                      password: str = Form(...),
-                     verification_code: Optional[str] = Form(''),
+                     verification_code: Optional[str] = Form(""),
+                     settings: Optional[str] = Form(""),
                      clients: ClientStorage = Depends(get_clients)) -> str:
     """Login by username and password with 2FA
     """
     cl = clients.client()
+    if settings is not None and settings != "":
+        cl.set_settings(json.loads(settings))
+
     result = cl.login(
         username,
         password,
@@ -27,3 +30,22 @@ async def auth_login(username: str = Form(...),
         clients.set(cl)
         return cl.sessionid
     return result
+
+
+@router.post("/relogin")
+async def auth_relogin(sessionid: str,
+                       clients: ClientStorage = Depends(get_clients)) -> str:
+    """Relogin by username and password (with clean cookies)
+    """
+    cl = clients.get(sessionid)
+    result = cl.relogin()
+    return result
+
+
+@router.get("/settings")
+async def settings(sessionid: str,
+                   clients: ClientStorage = Depends(get_clients)) -> Dict:
+    """Get client's settings
+    """
+    cl = clients.get(sessionid)
+    return cl.get_settings()
