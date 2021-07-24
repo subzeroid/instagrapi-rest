1a2ba36bfc24be90d3092486ced14343ab4fa533
bedefaced <bedefaced@protonmail.com>
2021-07-25 00:02:04 +0300

    photo download

--- routers/photo.py
+++ routers/photo.py
@@ -1,13 +1,13 @@
 from typing import List, Optional
-
+from pathlib import Path
 import requests
 from pydantic import HttpUrl
 from fastapi import APIRouter, Depends, File, UploadFile, Form
+from fastapi.responses import FileResponse
 from instagrapi.types import (
     Story, StoryHashtag, StoryLink,
     StoryLocation, StoryMention, StorySticker
 )
-
 from helpers import photo_upload_story_as_video, photo_upload_story_as_photo
 from dependencies import ClientStorage, get_clients
 
@@ -85,3 +85,37 @@ async def photo_upload_to_story_by_url(sessionid: str = Form(...),
             hashtags=hashtags,
             locations=locations,
             stickers=stickers)
+
+
+
+@router.post("/download", response_model=Story)
+async def photo_download(sessionid: str = Form(...),
+                         media_pk: int = Form(...),
+                         folder: Optional[Path] = Form(""),
+                         returnFile: Optional[bool] = Form(True),
+                         clients: ClientStorage = Depends(get_clients)):
+    """Download photo using media pk
+    """
+    cl = clients.get(sessionid)
+    result = cl.photo_download(media_pk, folder)
+    if returnFile:
+        return FileResponse(result)
+    else:
+        return result
+
+
+@router.post("/download/by_url", response_model=Story)
+async def photo_download_by_url(sessionid: str = Form(...),
+                         url: str = Form(...),
+                         filename: Optional[str] = Form(""),
+                         folder: Optional[Path] = Form(""),
+                         returnFile: Optional[bool] = Form(True),
+                         clients: ClientStorage = Depends(get_clients)):
+    """Download photo using URL
+    """
+    cl = clients.get(sessionid)
+    result = cl.photo_download_by_url(url, filename, folder)
+    if returnFile:
+        return FileResponse(result)
+    else:
+        return result
